29403594eaf96c23f8389a68a03a134b38ab3b94
Felix Ohnesorge <felix.ohnesorge@8qfx1ai5.de>
2020-03-25 15:37:35 +0100

    add some Buttons and a list as prototype

--- app/src/main/java/de/fo_8qfx1ai5/keep_distance/ListBlueToothDevicesActivity.java
+++ app/src/main/java/de/fo_8qfx1ai5/keep_distance/ListBlueToothDevicesActivity.java
@@ -3,12 +3,21 @@ package de.fo_8qfx1ai5.keep_distance;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class ListBlueToothDevices extends AppCompatActivity {
+public class ListBlueToothDevicesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bluetooth_devices);
+
+        final Button button = (Button) findViewById(R.id.ListDevicesButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/de/fo_8qfx1ai5/keep_distance/MainActivity.java
+++ app/src/main/java/de/fo_8qfx1ai5/keep_distance/MainActivity.java
@@ -1,7 +1,10 @@
 package de.fo_8qfx1ai5.keep_distance;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.GoToBlueToothDevicesButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListBlueToothDevicesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
