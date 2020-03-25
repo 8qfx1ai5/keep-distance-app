7cb70908b7d0f9ae8a3b5c2d34d06dc75e479e43
Felix Ohnesorge <felix.ohnesorge@8qfx1ai5.de>
2020-03-25 13:54:30 +0100

    init commit with first changes

---
+++ app/src/main/java/de/fo_8qfx1ai5/keep_distance/ListBlueToothDevices.java
@@ -0,0 +1,14 @@
+package de.fo_8qfx1ai5.keep_distance;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListBlueToothDevices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bluetooth_devices);
+    }
+}
---
+++ app/src/main/java/de/fo_8qfx1ai5/keep_distance/MainActivity.java
@@ -0,0 +1,13 @@
+package de.fo_8qfx1ai5.keep_distance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
